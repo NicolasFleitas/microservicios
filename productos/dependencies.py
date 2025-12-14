7a03bdb65a0fc75a13d8e6ade28e4d349e5a3815
Nicolas Fleitas <nicofleitaspy@gmail.com>
2025-12-13 22:42:21 -0300

    feat (security) Implementar JWT verification en productos

--- productos/dependencies.py
+++ productos/dependencies.py
@@ -2,27 +2,33 @@ import os
 from dotenv import load_dotenv
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import jwt
 
 load_dotenv()
 
-API_TOKEN_SECRETO = os.getenv("SECRET_KEY")
-
 security = HTTPBearer()
+
+API_TOKEN_SECRETO = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
 
 async def validar_token(credenciales: HTTPAuthorizationCredentials = Depends(security)):
     """
     Dependencia de seguridad.
-    1. HTTPBearer se encarga de verificar que venga el header 'Authorization'.
-    2. Se encarga de verificar que empiece con 'Bearer '.
-    3. Nos entrega el token limpio en credenciales.credentials.
+    Decodifica y valida el token JWT.
     """
     token_recibido = credenciales.credentials
+    exception_auth = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Credenciales inválidas. Token no válido.",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
 
-    if token_recibido != API_TOKEN_SECRETO:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Credenciales inválidas. Acceso denegado.",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    try:
+        payload = jwt.decode(token_recibido, API_TOKEN_SECRETO, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise exception_auth
+    except jwt.InvalidTokenError:
+        raise exception_auth
     
     return token_recibido
